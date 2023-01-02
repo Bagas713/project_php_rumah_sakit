59c15881c3085466bacb47fcd4dabdfce6fe0ddc
vendot <fikrialjauzi1@gmail.com>
2023-01-02 14:26:09 +0700

    history pasien frontend selesai

---
+++ history-pasien.php
@@ -0,0 +1,82 @@
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Id Pasien</th>
+      <th scope="col">Nama</th>
+      <th scope="col">No CM</th>
+      <th scope="col">No CM2</th>
+      <th scope="col">Tanggal Lahir</th>
+      <th scope="col">Umur</th>
+      <th scope="col">Tanggal Registrasi</th>
+      <th scope="col">Alamat CM</th>
+      <th scope="col">Alamat</th>
+      <th scope="col">Provinsi</th>
+      <th scope="col">Pendidikan</th>
+      <th scope="col">Pekerjaan</th>
+      <th scope="col">No Telp</th>
+      <th scope="col">Pasien Type</th>
+      <th scope="col">Provinsi User</th>
+      <th scope="col">Status</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td >Larry</td>
+      <td >-</td>
+      <td>@twitter</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+      <td>-</td>
+    </tr>
+  </tbody>
+</table>
