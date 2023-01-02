2ce6af6b9415d281dc4217044f99a516fa217857
vendot <fikrialjauzi1@gmail.com>
2023-01-02 14:27:20 +0700

    hampir selesai

---
+++ petunjuk-pengisian.php
@@ -0,0 +1,97 @@
+<div class="container-content">
+  <div class="title">
+    <h2>Petunjuk Pengisian</h2>
+  </div>
+
+  <div class="title-content">
+    <h3>Identitas</h3>
+    <div class="hint-box">
+      </div><div class="hint-box">
+        <div class="title-box">
+          <p>No RM</p>
+        </div>
+        <div class="isi">
+          <p>xxxxxxxxx</p>
+        </div>
+      <div class="title-box">
+        <p>Nama</p>
+      </div>
+      <div class="isi">
+        <p>Sesuai KTP, tanpa gelar, tanpa panggilan</p>
+      </div>
+    </div>
+    <div class="hint-box">
+      <div class="title-box">
+        <p>Identitas Pasien</p>
+      </div>
+      <div class="isi">
+        <p>No KTP</p>
+      </div>
+    </div><div class="hint-box">
+      <div class="title-box">
+        <p>Tanggal Lahir</p>
+      </div>
+      <div class="isi">
+        <p>Tanggal lahir pasien, isian didahului dengan tahun, bulan dan tanggal, save atau done. bila tanggal lahir tidak diketahui, dipakai tanggal 31 Desember.</p>
+      </div>
+    </div><div class="hint-box">
+      <div class="title-box">
+        <p>Umur</p>
+      </div>
+      <div class="isi">
+        <p>Umur pasien akan otomatis terisi saat user mengisi tanggal lahir yang benar</p>
+      </div>
+    </div><div class="hint-box">
+      <div class="title-box">
+        <p>Pendidikan</p>
+      </div>
+      <div class="isi">
+        <p>Pendidikan tertinggi pasien tersebut</p>
+      </div>
+    </div><div class="hint-box">
+      <div class="title-box">
+        <p>Pekerjaan</p>
+      </div>
+      <div class="isi">
+        <p>Pekerjaan sekarang pasien tersebut</p>
+      </div>
+    </div><div class="hint-box">
+      <div class="title-box">
+        <p>Alamat Sesuai KTP</p>
+      </div>
+      <div class="isi">
+        <p>Sesuai KTP, nama jalan, no rt dan rw</p>
+      </div>
+    </div><div class="hint-box">
+      <div class="title-box">
+        <p>Rumah Sakit Tempat Pelayanan</p>
+      </div>
+      <div class="isi">
+        <p>Kesepakatan adalah RS pelayanan pertama pasien tersebut, atau ada alamat lain yg lebih mudah dihubungi</p>
+      </div>
+    </div><div class="hint-box">
+      <div class="title-box">
+        <p>No Telp Pasien</p>
+      </div>
+      <div class="isi">
+        <p>No Telpon Pasien</p>
+      </div>
+    </div><div class="hint-box">
+      <div class="title-box">
+        <p>No Telp Keluarga Dekat</p>
+      </div>
+      <div class="isi">
+        <p>No Telpon Keluarga Dekat</p>
+      </div>
+    </div><div class="hint-box">
+      <div class="title-box">
+        <p>Jenis Kasus</p>
+      </div>
+      <div class="isi">
+        <p>Jenis kasus pasien</p>
+      </div>
+    </div>
+
+    <h5>Apabila telah selesai mengisi data tersebut user harus memilih tombol “Save Identitas” untuk menyimpan data yang telah dimasukan.</h5>
+  </div>
+</div>
