e69da4bdc5b3de655c56f3533e81a394deb91060
vendot <fikrialjauzi1@gmail.com>
2023-01-02 14:25:51 +0700

    data pasien frontend selesai

---
+++ data-pasien.php
@@ -0,0 +1,8 @@
+<div class="container-content">
+  <div class="ti">
+    <h2>Data Pasien</h2>
+  </div>
+  <div class="input-group rounded">
+    <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+  </div>
+</div>
