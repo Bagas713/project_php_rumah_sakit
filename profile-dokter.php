07337c4062607c50a755b7fd949d7dc49292783c
vendot <fikrialjauzi1@gmail.com>
2023-01-03 00:00:53 +0700

    frontend done

---
+++ profile-dokter.php
@@ -0,0 +1,50 @@
+<form style="width: 1000px; margin: auto;">
+  <div class="form-group row">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">First Name</label>
+    <div class="col-sm-9">
+      <input type="text" class="form-control" id="inputEmail3" placeholder="First Name">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Last Name</label>
+    <div class="col-sm-9">
+      <input type="text" class="form-control" id="inputPassword3" placeholder="Last Name">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Phone</label>
+    <div class="col-sm-9">
+      <input type="text" class="form-control" id="inputPassword3" placeholder="Phone">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Title</label>
+    <div class="col-sm-9">
+      <input type="text" class="form-control" id="inputPassword3" placeholder="Title">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Singkatan</label>
+    <div class="col-sm-9">
+      <input type="text" class="form-control" id="inputPassword3" placeholder="Singkatan">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password (If Changing Password)</label>
+    <div class="col-sm-9">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="New Password">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Confirm Password</label>
+    <div class="col-sm-9">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Confirm New Password">
+    </div>
+  </div>
+
+  <div class="form-group row">
+    <div class="col-sm-9">
+      <button type="submit" class="btn btn-primary">Save</button>
+    </div>
+  </div>
+</form>
